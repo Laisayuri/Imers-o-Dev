efaa273fcacd058d0ada9420f9d3a77dd1fa10bb
Lais <lais.yasunaka@gmail.com>
2022-03-12 20:56:00 -0300

    conversor de moeda

---
+++ 01ConversorDeMoedas/function.js
@@ -0,0 +1,11 @@
+function Converter () {
+  
+    var valorElemento = document.getElementById("valor");
+    var valor = valorElemento.value;
+    var valorDolar = parseFloat (valor);
+    var valorReal = valorDolar * 5;
+    var elementoValorConvertido = document.getElementById("valorConvertido");
+    var valorConvertido = "O valor em real é: R$ " + valorReal;
+    elementoValorConvertido.innerHTML = valorConvertido;
+    
+  }
