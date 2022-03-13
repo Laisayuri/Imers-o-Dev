0d4b63f2b2381b7a392e95d115d0aefeaa074a71
Lais <lais.yasunaka@gmail.com>
2022-03-12 21:15:52 -0300

    update

---
+++ 01_Calculadora/function.js
@@ -0,0 +1,28 @@
+function calculaMedia(){
+    var notaPrimeiroBimestre = parseInt(document.getElementById("nota1").value);
+    var notaSegundoBimestre = parseInt(document.getElementById("nota2").value);
+    var notaTerceiroBimestre = parseInt(document.getElementById("nota3").value);
+    var notaQuartoBimestre = parseInt(document.getElementById("nota4").value);
+    
+    var media = (((notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4).toFixed(1))
+    
+    if (media <= 5 ){
+      var status = "Reprovado"
+    } else {
+      var status = "Aprovado"
+    }
+    
+    document.getElementById("resultado").innerHTML = "<label id='resultado'>Sua média foi = " + media + " você foi " + status + "</label>"
+  }
+  
+  
+  // var nome = "Beth"
+  
+  // console.log("bem vinda" + nome)
+  // var notaDoPrimeiroTrimestre = 6
+  // var notaDoSegundoTirmestre = 8
+  // var notaDoTerceiroTrimestre = 4
+  // var notaFinal = (notaDoPrimeiroTrimestre + notaDoSegundoTirmestre + notaDoTerceiroTrimestre) / 4
+  // var notaFixada = notaFinal.toFixed (1)
+  // console.log(notaFixada)
+  
